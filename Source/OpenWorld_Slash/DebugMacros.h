c62418f10ae74bdf6bae70e1e95857c97b89157c
Erden <erdenutku07@gmail.com>
2022-12-28 22:42:30 -0500

    DebugMacros.h has been created.

---
+++ Source/OpenWorld_Slash/DebugMacros.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "DrawDebugHelpers.h"
+
+#define DEBUG_PRINT_SCREEN(Key, Message) if(GEngine)  GEngine->AddOnScreenDebugMessage(Key, 30.f, FColor::Magenta, Message);
+
+#define DEBUG_DRAW_SPHERE(Location, Color) if(GetWorld())  DrawDebugSphere(GetWorld(), Location, 25.f, 12, Color, true);
+
+#define DEBUG_DRAW_LINE(StartLoc, EndLoc, Color) if(GetWorld())  DrawDebugLine(GetWorld(), StartLoc, EndLoc, Color, true, -1, 0, 1);
+#define DEBUG_DRAW_POINT(Location, Color) if(GetWorld())  DrawDebugPoint(GetWorld(), Location, 15.f, Color, true);
+#define DEBUG_DRAW_VECTOR(StartLoc, EndLoc, Color)  if(GetWorld()) \
+{ \
+DEBUG_DRAW_LINE(StartLoc, EndLoc, Color) \
+DEBUG_DRAW_POINT(EndLoc, Color)\
+}
+
+
--- Source/OpenWorld_Slash/Private/Items/Item.cpp
+++ Source/OpenWorld_Slash/Private/Items/Item.cpp
@@ -3,6 +3,10 @@
 
 #include "Items/Item.h"
 
+#include "OpenWorld_Slash/DebugMacros.h"
+
+
+
 AItem::AItem()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,12 +16,17 @@ AItem::AItem()
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("HOLA!"));
+	//UE_LOG(LogTemp, Warning, TEXT("HOLA!"));
+	const FVector Location = GetActorLocation();
+	DEBUG_DRAW_SPHERE(Location, FColor::Blue)
+	DEBUG_DRAW_VECTOR(Location, Location + GetActorForwardVector()*100.f, FColor::Blue)
 }
 
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	//const FString Message = FString::Printf(TEXT("DELTATIME: %f"), DeltaTime);
+	//DEBUG_PRINT_SCREEN(2, Message)
+	
 }
 
